7bef1a14fe2c1cb1959cdc85929edaaadd566438
Yohann Pirbay <pirbay.yohann@gmail.com>
2023-10-09 21:25:06 +0100

    Resolve invalid sequence

--- employee.py
+++ employee.py
@@ -37,7 +37,7 @@ class Employee:
             answer += f" and receives a bonus commission of {self.commission}"
         elif (self.commissionType == "contract"):
             answer += f" and receives a commission for {self.n_contracts} contract(s) at {self.commission}/contract"
-        answer += "."
+        answer += f"."
         answer += f" Their total pay is {self.get_pay()}."
         return answer
 
@@ -59,6 +59,4 @@ robbie = Employee('Robbie', "monthly", 2000, commissionType="bonus", commission=
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel', "hourly", 30, 120, "bonus", 600)
-
-
 
